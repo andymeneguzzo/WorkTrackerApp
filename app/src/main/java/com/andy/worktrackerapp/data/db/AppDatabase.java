8722131db9b99d1e779f45480d9ae037e5cb6fea
Andy <andres.meneguzzo@gmail.com>
2024-12-27 23:30:39 +0100

    Updated files

--- app/src/main/java/com/andy/worktrackerapp/data/db/AppDatabase.java
+++ app/src/main/java/com/andy/worktrackerapp/data/db/AppDatabase.java
@@ -8,7 +8,7 @@ import android.content.Context;
 import com.andy.worktrackerapp.data.dao.ShiftDao;
 import com.andy.worktrackerapp.data.model.Shift;
 
-@Database(entities = {Shift.class}, version = 1, exportSchema = false)
+@Database(entities = {Shift.class}, version = 2, exportSchema = false) // Incrementa la versione da 1 a 2
 public abstract class AppDatabase extends RoomDatabase {
 
     private static AppDatabase instance;
@@ -22,7 +22,7 @@ public abstract class AppDatabase extends RoomDatabase {
                             AppDatabase.class,
                             "shifts_db"
                     )
-                    .fallbackToDestructiveMigration() // Rimuovi per gestire migrazioni
+                    .fallbackToDestructiveMigration() // Mantieni questo metodo per migrazioni distruttive
                     .build();
         }
         return instance;

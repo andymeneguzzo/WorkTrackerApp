4e3a16c365e078edafd2d7ba7fb503de85877ee7
Andy <andres.meneguzzo@gmail.com>
2024-12-27 17:58:36 +0100

    Updated files

--- app/src/main/java/com/andy/worktrackerapp/data/model/Shift.java
+++ app/src/main/java/com/andy/worktrackerapp/data/model/Shift.java
@@ -3,6 +3,20 @@ package com.andy.worktrackerapp.data.model;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+@Entity(tableName = "shifts")
 public class Shift {
 
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String date;
+    private double hoursWorked;
+    private double hourlyWage;
+
+    // Costruttore
+    public Shift(String date, double hoursWorked, double hourlyWage) {
+        this.date = date;
+        this.hoursWorked = hoursWorked;
+        this.hourlyWage = hourlyWage;
+    }
 }

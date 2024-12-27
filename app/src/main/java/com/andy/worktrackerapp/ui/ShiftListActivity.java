b8575cd6a24b87dc91446c2ba55f0d57746d23e0
Andy <andres.meneguzzo@gmail.com>
2024-12-27 18:54:14 +0100

    Updated files

--- app/src/main/java/com/andy/worktrackerapp/ui/ShiftListActivity.java
+++ app/src/main/java/com/andy/worktrackerapp/ui/ShiftListActivity.java
@@ -14,4 +14,33 @@ import com.andy.worktrackerapp.data.model.Shift;
 import com.andy.worktrackerapp.data.repository.ShiftRepository;
 
 public class ShiftListActivity extends AppCompatActivity {
+    private ListView listViewShifts;
+    private ShiftRepository shiftRepository;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shift_list);
+
+        listViewShifts = findViewById(R.id.listViewShifts);
+        shiftRepository = new ShiftRepository(this);
+
+        List<Shift> allShifts = shiftRepository.getAllShifts();
+
+        List<String> displayList = new ArrayList<>();
+        for(Shift shift : allShifts) {
+            String item = "Data: " + shift.getDate() +
+                    "\nOre: " + shift.getHoursWorked() +
+                    "\nPaga oraria: " + shift.getHourlyWage() +
+                    "\nTotale turno: " + shift.getTotalPay() + " €";
+            displayList.add(item);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                displayList
+        );
+        listViewShifts.setAdapter(adapter);
+    }
 }

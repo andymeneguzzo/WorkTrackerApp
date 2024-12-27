c55f0316b228633d157bbdf460c77439110db9ac
Andy <andres.meneguzzo@gmail.com>
2024-12-27 18:14:04 +0100

    Updated files

--- app/src/main/java/com/andy/worktrackerapp/data/db/AppDatabase.java
+++ app/src/main/java/com/andy/worktrackerapp/data/db/AppDatabase.java
@@ -1,4 +1,29 @@
 package com.andy.worktrackerapp.data.db;
 
-public class AppDatabase {
+import androidx.room.Database;
+import androidx.room.Entity;
+import androidx.room.RoomDatabase;
+import androidx.room.Room;
+import android.content.Context;
+
+import com.andy.worktrackerapp.data.dao.ShiftDao;
+import com.andy.worktrackerapp.data.model.Shift;
+
+@Database(entities = {Shift.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+    private static AppDatabase instance;
+    public abstract ShiftDao shiftDao();
+
+    public static synchronized AppDatabase getInstance(Context context) {
+        if(instance == null) {
+            instance = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    AppDatabase.class,
+                    "shifts_db"
+            ).allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
 }

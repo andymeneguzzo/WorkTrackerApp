6e72b0e847f8a9fd3a6568582f92deb3108a2444
Andy <andres.meneguzzo@gmail.com>
2024-12-27 19:25:22 +0100

    Updated files

--- app/src/main/java/com/andy/worktrackerapp/ui/StatsActivity.java
+++ app/src/main/java/com/andy/worktrackerapp/ui/StatsActivity.java
@@ -1,4 +1,45 @@
 package com.andy.worktrackerapp.ui;
 
-public class StatsActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import com.andy.worktrackerapp.R;
+import com.andy.worktrackerapp.data.model.Shift;
+import com.andy.worktrackerapp.data.repository.ShiftRepository;
+
+public class StatsActivity extends AppCompatActivity {
+    private EditText etMonth, etYear;
+    private Button btnMonthlyStats, btnYearStats, btnTotalStats;
+    private TextView tvResults;
+
+    private ShiftRepository shiftRepository;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+
+        etMonth = findViewById(R.id.etMonth);
+        etYear = findViewById(R.id.etYear);
+        btnMonthlyStats = findViewById(R.id.btnMonthlyStats);
+        btnYearStats = findViewById(R.id.btnYearlyStats);
+        btnTotalStats = findViewById(R.id.btnTotalStats);
+        tvResults = findViewById(R.id.tvResults);
+
+        shiftRepository = new ShiftRepository(this);
+
+        // Statistiche mensili
+        // Statistiche annuali
+        // Statistiche totali
+
+        // TODO da fare i metodi sopra
+    }
 }
